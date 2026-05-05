d6718266b2ae42fae39a6be1ec0c90522b228cc5
idorleen <3402419979@qq.com>
2026-05-05 11:43:31 +0800

    'UserCenter未知报错不影响编译运行，无语法逻辑错误'

--- src/views/user/UserCenter.vue
+++ src/views/user/UserCenter.vue
@@ -139,6 +139,7 @@
       </div>
     </div>
 
+    <!-- 签到弹窗 -->
     <el-dialog
       v-model="checkinDialogVisible"
       title="活动签到"
@@ -267,7 +268,7 @@ const avatarUrl = computed(() => {
 })
 
 const handleAvatarError = (event) => {
-  event.target.src = 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'
+  event.target.src = 'http://localhost:8080/api/upload/default.png'
 }
 
 onMounted(async () => {
